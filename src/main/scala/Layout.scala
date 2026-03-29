e020ebfbdeca8f5ffbd7c92d75de3c6d62370516
Kazuhiro Funakoshi <steelheart.wolverine@gmail.com>
2026-03-29 14:31:10 -0700

    refactor(Strokes): now reads original T-Code table from tc2 impl

--- src/main/scala/Layout.scala
+++ src/main/scala/Layout.scala
@@ -1,22 +1,20 @@
 package io.github.nicheapplab.t_codeengine
 
 abstract trait Layout{
-  def getStroke(c: Char): Option[Char]
+  def getStroke(c: Char): Option[Int]
 }
 
 /** Provides validation of Qwerty keys stroke for T-Code */
 trait QwertyLayout extends Layout{
-  import QwertyLayout._
-
   /**
     * Returns a char that can be part of the key stroke of T-Code
     *
     * @param c a character typed with a Qwerty keyboard layout
-    * @return an Option of a char can be part of the key stroke of T-Code if valid, otherwise None
+    * @return an Option of a Int which indicates key number if valid
     */
-  def getStroke(c: Char): Option[Char] = {
-    if (left.contains(c) || right.contains(c)){
-      Some(c)
+  def getStroke(c: Char): Option[Int] = {
+    if (QwertyLayout.keyseq.contains(c)) {
+      Some (QwertyLayout.keyseq.indexWhere(_ == c))
     } else {
       None
     }
@@ -24,48 +22,26 @@ trait QwertyLayout extends Layout{
 }
 /** Defines the keyboard layout of QwertyLayout */
 object QwertyLayout{
-  val left = "12345qwertasdfgzxcvb"
-  val right = "67890yuiophjkl;nm,./"
-
-  /**
-    * Given a internal Int presentation of a stroke, find the key when in a Qwerty keyboard.
-    * This is used for keyboard layouts other than Qwerty
-    * @param stroke Qwerty presentation of a key stroke
-    * @return Qwerty key representation of the stroke if valid. Otherwise None.
-    */
-  def getQwertyChar(stroke: Int): Option[Char] = {
-    if(0 <= stroke && stroke <= 39){
-      Some( (left+right).charAt(stroke) )
-    } else None
-  }
+  val keyseq = "1234567890qwertyuiopasdfghjkl;zxcvbnm,./"
 }
 
 /** Provides validation of Dvorak key stroke for T-Code */
 trait DvorakLayout extends Layout{
-  import DvorakLayout._
   /**
-    * Returns a char that can be part of the key stroke of T-Code
+    * Returns a key number that can be part of the key stroke of T-Code
     *
     * @param c: A character typed with a Dvorak keyboard layout
-    * @return an Option of a char in Qwerty keyboard layout presentation after validation.
-    * It can be part of the key stroke of T-Code if valid, otherwise None
+    * @return an Option of a Int which indicates key number if valid
     */
-  def getStroke(c: Char): Option[Char] = {
-    val k: Option[Int] = if (left.contains(c)) {
-      Some(left.indexWhere(_ == c))
-    } else if (right.contains(c)) {
-      Some(left.indexWhere(_ == c) + 20)
-    } else {
-      None
+  def getStroke(c: Char): Option[Int] = {
+      if (DvorakLayout.keyseq.contains(c)){
+        Some(DvorakLayout.keyseq.indexWhere(_ == c) )
+      } else {
+        None
+      }
     }
-    k match{
-      case Some(v) => QwertyLayout.getQwertyChar(v)
-      case None => None
-    }
-  }
 }
 /** Defines the keyboard layout of DvorakLayout */
 object DvorakLayout{
-  val left = "12345\',.pyaoeui;qjkx"
-  val right = "67890fgcrldhtnsbmwvz"
+  val keyseq = "1234567890\',.pyfgcrlaoeuidhtns;qjkxbmwvz"
 }
