bf4a8b2ea85d363ed280fe1e6db4f8a340164dff
Kazuhiro Funakoshi <steelheart.wolverine@gmail.com>
2026-05-01 13:11:56 -0700

    added test fixtures

--- src/test/scala/EELLLTXT.scala
+++ src/test/scala/EELLLTXT.scala
@@ -2,7 +2,7 @@
 // https://scalameta.org/munit/docs/getting-started.html
 package io.github.nicheapplab.tcodeengine
 
-class EELLLTXT0 extends munit.FunSuite {
+class EELLLTXT0 extends BatchEngineFixture {
 
   case class LessonData(name: String, expected: Array[String], strokes: Array[String])
 
@@ -399,15 +399,10 @@ class EELLLTXT0 extends munit.FunSuite {
     test(lesson.name) {
       // Ignore tests where strokes haven't been implemented yet
       assume(!lesson.strokes.contains("TODO"), s"T-Code strokes missing for ${lesson.name}")
-
-      val tcode_tbl_path = System.getProperty("java.io.tempdir") ++ "/.t-code-engine/tcode_tbl.db"
-      val mazegaki_path = System.getProperty("java.io.tempdir") ++ "/.t-code-engine/mazegaki.db"
-      val bushu_path = System.getProperty("java.io.tempdir") ++ "/.t-code-engine/bushu.db"
-      val jdbc_prefix = "jdbc:sqlite"
-      val engine = new SQLiteBatchEngine(jdbc_prefix, tcode_tbl_path, mazegaki_path, bushu_path) with QwertyLayout with SQLiteStrokes
+      val be = engine()
 
       val result_array = lesson.strokes.map(str =>
-        engine.convert(str)
+        be.convert(str)
       )
       val comp = result_array.zip(lesson.expected)
       comp.foreach((a,b) => assertEquals(a,b))
--- src/test/scala/MixedConverterTest.scala
+++ src/test/scala/MixedConverterTest.scala
@@ -2,19 +2,18 @@
 // https://scalameta.org/munit/docs/getting-started.html
 package io.github.nicheapplab.tcodeengine
 
-class MixedConverterTest extends munit.FunSuite {
-  val mazegaki_path = System.getProperty("java.io.tempdir") ++ "/.t-code-engine/mazegaki.db"
-  val jdbc_prefix = "jdbc:sqlite"
-
-  val mixed = new MixedConverter with SQLiteMixedConverterDictionary(jdbc_prefix, mazegaki_path)
+class MixedConverterTest extends BatchEngineFixture {
 
   test("記しゃ"){
+    val mixed = engine().mixed
     assertEquals(mixed.convert("記しゃ").contains("記者"), true)
   }
   test("花だん"){
+    val mixed = engine().mixed
     assertEquals(mixed.convert("花だん").contains("花壇"), true)
   }
   test("加える"){
+    val mixed = engine().mixed
     val candidates = mixed.convert("くわえ","る")
     assertEquals(candidates.toList, List("加える"))
   }
