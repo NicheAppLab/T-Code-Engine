83f194653ec0404f24d93e798345ece729a026b6
Kazuhiro Funakoshi <steelheart.wolverine@gmail.com>
2026-05-09 16:46:45 -0700

    refactored layout

--- src/main/scala/Layout.scala
+++ src/main/scala/Layout.scala
@@ -1,58 +1,26 @@
 package io.github.nicheapplab.tcodeengine
 
 abstract trait Layout{
-  def getStroke(c: Char): Option[Int]
-  def getKey(i: Int): Option[Char]
-}
-
-/** Provides validation of Qwerty keys stroke for T-Code */
-trait QwertyLayout extends Layout{
-  /**
-    * Returns a char that can be part of the key stroke of T-Code
-    *
-    * @param c a character typed with a Qwerty keyboard layout
-    * @return an Option of a Int which indicates key number if valid
-    */
+  val keyseq: Seq[Char]
   def getStroke(c: Char): Option[Int] = {
-    if (QwertyLayout.keyseq.contains(c)) {
-      Some (QwertyLayout.keyseq.indexWhere(_ == c))
+    if (keyseq.contains(c)) {
+      Some (keyseq.indexWhere(_ == c))
     } else {
       None
     }
   }
   def getKey(i: Int): Option[Char] = {
     if (i<40){
-      Some(QwertyLayout.keyseq(i))
+      Some(keyseq(i))
     } else None
   }
-}
-/** Defines the keyboard layout of QwertyLayout */
-object QwertyLayout{
-  val keyseq = "1234567890qwertyuiopasdfghjkl;zxcvbnm,./"
 }
 
+/** Provides validation of Qwerty keys stroke for T-Code */
+trait QwertyLayout extends Layout{
+  val keyseq = "1234567890qwertyuiopasdfghjkl;zxcvbnm,./"
+}
 /** Provides validation of Dvorak key stroke for T-Code */
 trait DvorakLayout extends Layout{
-  /**
-    * Returns a key number that can be part of the key stroke of T-Code
-    *
-    * @param c: A character typed with a Dvorak keyboard layout
-    * @return an Option of a Int which indicates key number if valid
-    */
-  def getStroke(c: Char): Option[Int] = {
-      if (DvorakLayout.keyseq.contains(c)){
-        Some(DvorakLayout.keyseq.indexWhere(_ == c) )
-      } else {
-        None
-      }
-  }
-  def getKey(i: Int): Option[Char] = {
-    if (i<40) {
-      Some(DvorakLayout.keyseq(i))
-    } else None
-  }
-}
-/** Defines the keyboard layout of DvorakLayout */
-object DvorakLayout{
   val keyseq = "1234567890\',.pyfgcrlaoeuidhtns;qjkxbmwvz"
 }
